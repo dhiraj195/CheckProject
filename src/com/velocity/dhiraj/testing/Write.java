aec75e4f8d22da161f9be327e055d3cf4bf6fcaf
dhiraj <varpemahesh12@gmail.com>
2023-01-18 13:33:02 +0530

    Added Write

--- src/com/velocity/dhiraj/testing/Write.java
+++ src/com/velocity/dhiraj/testing/Write.java
@@ -10,8 +10,11 @@ public class Write {
 		list.add(259);
 		list.add("Varape");
 		list.remove(0);
+		list.add(0, "Welcome");
+		
+		list.add(24);
+		list.add("Java");
 		System.out.println(list);
-
 	}
 
 }
