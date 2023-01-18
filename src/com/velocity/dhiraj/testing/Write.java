4d2311e238cf8f65e58956bf029c9db7ed54f299
dhiraj <varpemahesh12@gmail.com>
2023-01-18 12:22:45 +0530

    Added Write class

---
+++ src/com/velocity/dhiraj/testing/Write.java
@@ -0,0 +1,15 @@
+package com.velocity.dhiraj.testing;
+
+import java.util.ArrayList;
+
+public class Write {
+
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add("Dhiraj");
+		list.add(259);
+		list.add("Varape");
+
+	}
+
+}
